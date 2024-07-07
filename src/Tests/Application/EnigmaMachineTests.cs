fb6b5aa9e27007e36f74a4e984f98baacd5749e0
Adam Avery Cole <adamaverycole@gmail.com>
2024-07-07 23:55:34 +0000

    Add single failing AAAAA enigma machine test.

--- src/Tests/Application/EnigmaMachineTests.cs
+++ src/Tests/Application/EnigmaMachineTests.cs
@@ -18,9 +18,23 @@ public class EnigmaMachineTests
         sut.ConfigureReflector(reflector);
     }
 
+    /// <summary>
+    /// > With the rotors I, II, III (from left to right), wide B-reflector, all ring settings in B-position, and start position AAA, typing AAAAA will produce the encoded sequence EWTYX.
+    /// <see href="https://en.wikipedia.org/wiki/Enigma_rotor_details#Ring_setting">source</see>.
+    /// </summary>
     [Test]
-    public void Test1()
+    public void TypeMessage_WhenPlainTextIsAAAAA_ShouldGetCipherEWTYX()
     {
-        Assert.Pass();
+        const string plainText = "AAAAA";
+        const string expectedCipher = "EWTYX";
+        
+        sut = new EnigmaMachine(new RotorAssembly());
+        sut.ConfigureReflector(new ReflectorB());
+        sut.ConfigureWheelOrder(new RotorI(), new RotorII(), new RotorIII(), null, null);
+        sut.ConfigureRingSettings('B', 'B', 'B', null, null);
+        sut.ConfigureIndicatorGroups("AAA");
+        string cipherResponse = sut.TypeMessage(plainText);
+
+        Assert.That(cipherResponse, Is.EqualTo(expectedCipher));
     }
 }
